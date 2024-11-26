8a1313a0b15ec2d3ca396364e77b0f5ba71ea8eb
Eve <pedromarinho1551@gmail.com>
2024-11-26 18:15:50 -0400

    Módulo 1 - C
    comitando nota 1 e respectivos códigos

--- Módulo 1 - C/hello.c
+++ Módulo 1 - C/hello.c
@@ -1,6 +1,6 @@
-#include <stdio.h>
+#include <stdio.h> /* Biblioteca Padrão */
 
-int main(void)
+int main(void) /*  */
 {
-    printf("Hello, World\n"); /* exibe um texto no terminal */
+    printf("Hello, World\n"); /* exibe um texto no terminal | \n pula para próxima linha */
 }
---
+++ Módulo 1 - C/helloyou.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <cs50.h> /* Biblioteca da CS50 */
+
+int main(void)
+{
+    string answer = get_string("What's your name? "); /*  */
+    printf("Hello, %s\n", answer); /*  */
+}
