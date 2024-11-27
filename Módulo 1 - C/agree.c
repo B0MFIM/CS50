fd8fd08d1a43994df6da6d88b5872f5443cd1b7d
Eve <pedromarinho1551@gmail.com>
2024-11-26 21:59:37 -0400

    Módulo 1 - C
    Atualizando notas e novos códigos

---
+++ Módulo 1 - C/agree.c
@@ -0,0 +1,16 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    char c = get_char("Do you agree? "); /* Recebe um unico caractere e aloca na variável c */
+
+    if (c == 'y' || c == 'Y') /* || significa "ou", assim como && significa "e" */
+    {
+        printf("Agreed.\n");
+    }
+    else if (c == 'n' || c == 'N')
+    {
+        printf("Not agreed.\n");
+    }
+}
---
+++ Módulo 1 - C/meow.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    /*
+
+    int i = 1;
+    while (i <= 3)  -> um loop while com uma expressão booleana (traduzindo: Enquanto i for menor ou igual a 3, continue)
+    {
+        printf("meow\n");
+        i++;        -> incrementa +1 ao valor de i
+    }
+    
+    */
+
+    for (int i = 0; i < 3; i++) /* o primeiro argumento inicia uma variável, a segundo é um valor booleano e o terceiro é o update da variável */
+    {
+            printf("meow\n");
+    }
+}
