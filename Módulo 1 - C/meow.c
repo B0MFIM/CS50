053993ed24028bdfcfd55d39d03737da4d9d8d4f
Eve <pedromarinho1551@gmail.com>
2024-11-26 22:54:38 -0400

    Módulo 1 - C
    Atualizando nota e códigos

--- Módulo 1 - C/meow.c
+++ Módulo 1 - C/meow.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <cs50.h>
 
+void meow(int n); /* Informa ao compilador que a função existe */
+
 int main(void)
 {
     /*
@@ -12,10 +14,21 @@ int main(void)
         i++;        -> incrementa +1 ao valor de i
     }
     
-    */
-
-    for (int i = 0; i < 3; i++) /* o primeiro argumento inicia uma variável, a segundo é um valor booleano e o terceiro é o update da variável */
+    for (int i = 0; i < 3; i++) -> o primeiro argumento inicia uma variável, a segundo é um valor booleano e o terceiro é o update da variável
     {
             printf("meow\n");
+    }
+
+    */
+
+    meow(5);  /* aqui nos chamamos a função criada e adicionamos o seu valor de argumento do tipo inteiro */
+
+}
+
+void meow(int n) /* criamos uma função chamada meow, com um argumento "n" do tipo int, para receber um valor quando chamada */
+{
+    for (int i = 0; i < n; i++)
+    {
+        printf("meow\n"); /* dentro da função se localiza a sua funcionalidade, o que ela faz */
     }
 }
