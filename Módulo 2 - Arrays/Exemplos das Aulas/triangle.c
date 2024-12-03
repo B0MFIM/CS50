b5ad5960e2cf505a9dfbdf9a746af062de287b29
Eve <pedromarinho1551@gmail.com>
2024-12-02 21:47:46 -0400

    Update triangle.c
    add coments

--- Módulo 2 - Arrays/Exemplos das Aulas/triangle.c
+++ Módulo 2 - Arrays/Exemplos das Aulas/triangle.c
@@ -1,20 +1,25 @@
 #include <cs50.h>
 #include <stdio.h>
 
+// Variável Constante
 #define SIDES 3
 
+// Declaração de Função
 bool valid_triangle(float a, float b, float c);
 
 int main(void)
 {
+    // Recebe do usuário 3 valores positivos acima de zero dentro de uma matriz 'triangle'
     float triangle[SIDES];
     for (int i = 0; i < SIDES; i++)
     {
         triangle[i] = get_float("Size %i: ", i + 1);
     }
 
+    // Chamada da função 'valid_triangle' que recebe 3 parâmetros
     bool valid = valid_triangle(triangle[0], triangle[1], triangle[2]);
 
+    // Imprime na tela se o triangulo é verdadeiro ou falso
     if (valid == true)
     {
         printf("True Triangle\n");
@@ -25,6 +30,7 @@ int main(void)
     }
 }
 
+// Definição de Função que valida um triangulo hipotético, retornando 'true' ou 'false'
 bool valid_triangle(float a, float b, float c)
 {
     if ((a + b <= c) || (a + c <= b) || (b + c <= a))
