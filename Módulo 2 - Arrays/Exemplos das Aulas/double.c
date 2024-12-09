7f7a26613bcdf7b40d70eb262445a95adfdac323
Eve <pedromarinho1551@gmail.com>
2024-12-09 18:05:55 -0400

    Update double.c

--- Módulo 2 - Arrays/Exemplos das Aulas/double.c
+++ Módulo 2 - Arrays/Exemplos das Aulas/double.c
@@ -9,16 +9,20 @@ int main(void)
     // array 'sequence' com 'L' posições
     int sequence[L];
 
-    // atribuindo valor para a posição 0 do array
-    sequence[0] = 1;
-    // exibir o valor da posição 0 do array
-    printf("%i\n", sequence[0]);
-
-    // loop para acessar as outras 4 posições do array
-    for (int i = 1; i < L; i++)
+    // loop para acessar as outras posições do array
+    for (int i = 0; i < L; i++)
     {
-        // atribuindo um valor na posição 'i' do array
-        sequence[i] = sequence[i - 1] * 2; // o valor atribuido é o valor da posição anterior multiplicado por 2
+        if (i == 0)
+        {
+            // atribuindo valor para a posição 0 do array
+            sequence[i] = 1;
+        }
+        else
+        {
+            // atribuindo um valor na posição 'i' do array
+            // o valor atribuido é o valor da posição anterior multiplicado por 2
+            sequence[i] = sequence[i - 1] * 2;
+        }
         // exibir o valor da posição 'i'
         printf("%i\n", sequence[i]);
     }
