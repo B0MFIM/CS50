7c3e06d068828313a2a05c8d2d8135ea6f26878e
Eve <pedromarinho1551@gmail.com>
2025-02-03 19:50:23 +0000

    📝 Add comments and reorganize the factorial function definition

--- Módulo 3 - Algorithms/Exemplos das Aulas/factorial.c
+++ Módulo 3 - Algorithms/Exemplos das Aulas/factorial.c
@@ -1,17 +1,23 @@
 #include <stdio.h>
 
-int factorial(int n) {
-    if (n == 0) {
-        return 1;
-    } else {
-        return n * factorial(n - 1);
-    }
-}
+int factorial(int n);
 
 int main() {
+
+    // get user's input
     int number;
     printf("Enter a number: ");
     scanf("%d", &number);
+
+    // call the factorial function
     printf("Factorial of %d is %d\n", number, factorial(number));
     return 0;
+}
+
+int factorial(int n) {
+    if (n == 0) {   // base case
+        return 1;
+    } else {        // recursive case
+        return n * factorial(n - 1);
+    }
 }
