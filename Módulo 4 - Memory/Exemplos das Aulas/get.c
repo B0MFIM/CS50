48699ebc76cd307c2c5aba4a9ae09f2aeab7a2e8
Eve <pedromarinho1551@gmail.com>
2025-02-21 04:33:51 +0000

    📝 Add comments explaining the usage of scanf function and its precautions

--- Módulo 4 - Memory/Exemplos das Aulas/get.c
+++ Módulo 4 - Memory/Exemplos das Aulas/get.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+/* Esse código foi um exemplo de como podemos usar a função scanf para
+   obter entradas do usuário. O scanf é uma função da biblioteca padrão
+   de C que lê entradas do usuário e as armazena em variáveis. Além disso,
+   ele pode ser usado para ler strings, mas é importante ter cuidado ao 
+   usá-lo para evitar buffer overflows. */
+
 int main(void)
 {
     int n;
