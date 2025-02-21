4fc783b43085a7425ac5175b8e9fca83345165fd
Eve <pedromarinho1551@gmail.com>
2025-02-21 04:22:00 +0000

    📝 Add comments explaining the concept of garbage values and the behavior of uninitialized array elements

--- Módulo 4 - Memory/Exemplos das Aulas/garbage.c
+++ Módulo 4 - Memory/Exemplos das Aulas/garbage.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
 
+/* Esse código foi um exemplo de garbage values. Ele mostra que, quando
+   declaramos um array de inteiros, os valores dos elementos do array
+   não são inicializados. E como não inicializamos os valores dos elementos
+   do array, eles contêm valores aleatórios que estavam na memória antes
+   de o array ser alocado. */
+
 int main(void)
 {
+    // Declara um array de 1024 inteiros
     int scores[1024];
+
+    // Exibe os valores dos elementos do array, que são valores aleatórios
     for (int i = 0; i < 1024; i++)
     {
         printf("%i\n", scores[i]);
