560b6300cc27df33f58b84c147317bacd29b5040
Eve <pedromarinho1551@gmail.com>
2025-02-21 04:16:16 +0000

    📝 Add comments explaining the purpose of the code and correct array index comments

--- Módulo 4 - Memory/Exemplos das Aulas/memory.c
+++ Módulo 4 - Memory/Exemplos das Aulas/memory.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/* Esse código foi um exemplo de como Valgrind pode ser utilizado para
+   verificar se um programa tem vazamento de memória. */
+
 int main(void)
 {
     int *x = malloc(3 * sizeof(int));
-    x[0] = 72;
-    x[1] = 23;
-    x[2] = 33;
+    x[0] = 72; // x[1] = 72;
+    x[1] = 23; // x[2] = 23;
+    x[2] = 33; // x[3] = 33;
     free(x);
 }
